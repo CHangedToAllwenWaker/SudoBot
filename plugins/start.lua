d26fac64f7eddfb96ef3e357faf318ccf485d4bf
allwen <ArianGhasempour40@Gmail.com>
2016-06-24 03:09:36 -0700

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -1,47 +1,10 @@
-local function do_keyboard_robot()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-		{
-    					{text = 'Share Robot Contact 🤖🤘🏾', callback_data = '!share'},
-    					},
-    					{
-    		    		{text = 'Buy Group 💸', callback_data = '!buygroup'},
-    		    		{text = 'Support 👥', url = 'https://telegram.me/joinchat/BvytAD9KL7J2PE2u0ek3ZA'},
-	    },
-	    {
-	    {text = '🔙', callback_data = '!home'}
-        }
-    }
-    return keyboard
-end
-local function do_keyboard_buygroup()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-{
-    		    		{text = 'Iranians', url = 'http://salam.im/buy/ecgvlup3ld'},
-    		    		{text = 'Other countries', url = 'https://telegram.me/joinchat/BvytAD9KL7J2PE2u0ek3ZA'},
-	    },
-	    {
-	    {text = '🔙', callback_data = '!robot'}
-        }
-    }
-    return keyboard
-end
 local function do_keyboard_private()
     local keyboard = {}
     keyboard.inline_keyboard = {
     	{
-    		{text = '🌐 Site', url = 'http://beatbot.ir'},
-    		{text = '📡 Channels', callback_data = '!channel'},
-	    },
-		{
-	        {text = '📥 Contact Us 📤', callback_data = '/chat'},
-        },
-		{
-	        {text = 'About Us 👥', callback_data = '!aboutus'},
-        },
-	    {
-	        {text = '🔸BeatBotTG🔹', callback_data = '!robot'},
+
+	        {text = '�  Chat 📤', callback_data = '/chat'},
+                },
         }
     }
     return keyboard
@@ -56,35 +19,14 @@ local function do_keyboard_startme()
     }
     return keyboard
 end
-local function do_keyboard_channel()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-    	{
-    		{text = 'Persian Channel 🇮🇷', url = 'https://telegram.me/BeatBot_team'},
-	    },
-	{
-	        		{text = 'English Channel 🇬🇧', url = 'https://telegram.me/BeatBotTeam'},
-
-    },
-		{
-					{text = 'News Channel 🗣', url = 'https://telegram.me/BeatBot_News'},
-		},
-		{
-	    {text = '🔙', callback_data = '!home'},
-        }
-    
-    }
-    return keyboard
-end
-
 local action = function(msg, blocks, ln)
     if blocks[1] == 'start' or blocks[1] == 'help' then
         db:hset('bot:users', msg.from.id, 'xx')
         db:hincrby('bot:general', 'users', 1)
         if msg.chat.type == 'private' then
-            local message = [[📍 *Welcome BeatBotTeam Official Bot*  📍
+            local message = [[� Hi, I am sudo team official chat bot*  📍
 -------------------------------------------------------------
-🗣 `Please select an option ...`]]
+🗣 Please Select Inline Keyboard.`]]
             local keyboard = do_keyboard_private()
             api.sendKeyboard(msg.from.id, message, keyboard, true)
             end
@@ -94,40 +36,7 @@ local action = function(msg, blocks, ln)
         return
     end
 
-    if msg.cb then
-        local query = blocks[1]
-        local msg_id = msg.message_id
-        local text
-        if query == 'channel' then
-            local text = '📡 *BeatBotTeam Channels :*'
-            local keyboard = do_keyboard_channel()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
-end
-if query == 'robot' then
-            local text = [[🔸*BeatBotTG*🔹
-🚩 _An advanced robot for entertainment group manager and anti-spam_]]
-            local keyboard = do_keyboard_robot()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
-end
-if query == 'buygroup' then
-            local text = [[_Please wait after payment_ 
-_We will be call to you_]]
-            local keyboard = do_keyboard_buygroup()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
-end
-if query == 'home' then
-            local text = [[📍 *Welcome BeatBotTeam Official Bot*  📍
--------------------------------------------------------------
-🗣 `Please select an option ...`]]
-            local keyboard = do_keyboard_private()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
-end
-        if query == 'share' then
-     api.sendContact(msg.from.id, '+639380063518', '🔸ßελτ ßΘτ🔹 [ Use ! ]')
-end
-    end
-
-end
+    
 
 return {
 	action = action,
